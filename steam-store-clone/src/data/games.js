139e6d2e1a24612d9409f58a5e4396a4f6b072c8
ptzivaras <82588420+ptzivaras@users.noreply.github.com>
2025-11-04 19:18:02 +0200

    fix image display in tailwind
    only 2 images used

--- steam-store-clone/src/data/games.js
+++ steam-store-clone/src/data/games.js
@@ -1,18 +1,22 @@
+
+import game2 from "../images/game2.jpg";
+import game3 from "../images/game3.jpg";
+
 export const gamesData = {
   "New and Trending": [
-    { id: 1, name: "Void Hunters", price: 19.99, discount: 0.2, os: ["Windows"], image: "/assets/game1.jpg" },
-    { id: 2, name: "Solar Drift", price: 24.99, discount: 0.3, os: ["Windows", "Mac"], image: "/assets/game2.jpg" },
+    { id: 1, name: "Void Hunters", price: 19.99, discount: 0.2, os: ["Windows"], image: "/images/game2.jpg" },
+    { id: 2, name: "Solar Drift", price: 24.99, discount: 0.3, os: ["Windows", "Mac"], image: "/images/game3.jpg" },
   ],
   "Top Sellers": [
-    { id: 3, name: "Iron Age", price: 39.99, discount: 0.1, os: ["Windows"], image: "/assets/game3.jpg" },
-    { id: 4, name: "Neon City", price: 29.99, discount: 0.5, os: ["Windows", "Linux"], image: "/assets/game4.jpg" },
+    { id: 3, name: "Iron Age", price: 39.99, discount: 0.1, os: ["Windows"], image: "/images/game2.jpg" },
+    { id: 4, name: "Neon City", price: 29.99, discount: 0.5, os: ["Windows", "Linux"], image: "/images/game3.jpg" },
   ],
   "What's Being Played": [
-    { id: 5, name: "BattleForge", price: 49.99, discount: 0.25, os: ["Windows"], image: "/assets/game5.jpg" },
-    { id: 6, name: "Mystic Realms", price: 59.99, discount: 0.4, os: ["Windows", "Mac"], image: "/assets/game6.jpg" },
+    { id: 5, name: "BattleForge", price: 49.99, discount: 0.25, os: ["Windows"], image: "/images/game2.jpg" },
+    { id: 6, name: "Mystic Realms", price: 59.99, discount: 0.4, os: ["Windows", "Mac"], image: "/images/game3.jpg" },
   ],
   "Upcoming": [
-    { id: 7, name: "Chrono Frontier", price: 44.99, discount: 0, os: ["Windows"], image: "/assets/game7.jpg" },
-    { id: 8, name: "Sky Odyssey", price: 34.99, discount: 0, os: ["Windows", "Linux"], image: "/assets/game8.jpg" },
+    { id: 7, name: "Chrono Frontier", price: 44.99, discount: 0, os: ["Windows"], image: "/images/game2.jpg" },
+    { id: 8, name: "Sky Odyssey", price: 34.99, discount: 0, os: ["Windows", "Linux"], image: "/images/game3.jpg" },
   ],
 }
